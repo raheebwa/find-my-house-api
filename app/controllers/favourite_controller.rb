5f12d3f4780aa961f2b01d3170063827467b1e10
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-09-09 05:52:22 +0300

    feat: favourites controller

---
+++ app/controllers/favourite_controller.rb
@@ -0,0 +1,26 @@
+class FavouriteController < ApplicationController
+ def index
+    @favourite = Favourite.where('user_id = ?', params[:user_id])
+    json_response(@favourite)
+  end
+
+  def create
+    @favourite = current_user.favourites.create!(favourite_params)
+    json_response(@favourite, :created)
+  end
+
+  def destroy
+    set_favourite
+    @favourite.destroy
+  end
+
+  private
+
+  def favourite_params
+    params.permit(:house_id)
+  end
+
+  def set_favourite
+    @favourite = Favourite.find(params[:id])
+  end
+end
