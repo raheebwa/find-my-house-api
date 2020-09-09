a32a95b4d56fab0e843778b4c3289c92b75027ec
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-09-09 05:50:01 +0300

    feat: updated user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
     before_action :authorized, only: [:auto_login]
+    before_action :set_user, only: [:show, :update, :destroy]
+  
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
 
   # REGISTER
   def create
@@ -29,9 +44,27 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-  private
-
-  def user_params
-    params.permit(:username, :password)
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+  # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
