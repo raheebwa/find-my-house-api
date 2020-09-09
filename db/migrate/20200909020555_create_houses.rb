2e8503f6260c8c675e1a2416bf59dcf81fe87375
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-09-09 05:09:04 +0300

    chore: add houses model

---
+++ db/migrate/20200909020555_create_houses.rb
@@ -0,0 +1,12 @@
+class CreateHouses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.text :description
+      t.string :image_url
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
