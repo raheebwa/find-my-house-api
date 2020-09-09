08c777d59dd1075c9299a0be60ac9e1d135ec3cc
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-09-09 05:14:20 +0300

    feat: relationship house to others

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,5 @@
 class House < ApplicationRecord
+    has_many :favourites, dependent: :destroy
+    has_many :users, through: :favourites
+    validates_presence_of :name, :description, :image_url, :price
 end
