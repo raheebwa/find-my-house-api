375c5e65a55b0881a31438058522fa1d95aca363
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-09-09 06:10:16 +0300

    chore: add model specs

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe House, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:favourites).dependent(:destroy) }
+  it { should have_many(:users).through(:favourites) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:image_url) }
+  it { should validate_presence_of(:price) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:favourites).dependent(:destroy) }
+  it { should have_many(:houses).through(:favourites) }
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password) }
 end
