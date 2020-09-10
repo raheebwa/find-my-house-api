ac13dad0338f9d50d2ac14a38371ce31a422c7c4
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-09-10 16:33:42 +0300

    fix: user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     has_many :favourites, dependent: :destroy
     has_many :houses, through: :favourites
-    validates_presence_of :name, :email, :password_digest
+    validates_presence_of :username, :password_digest
 end
