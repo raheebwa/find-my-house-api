347d88349993ca45307580ed96c8182754476a88
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-09-11 10:41:54 +0300

    ...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.valid?
       token = encode_token({ user_id: @user.id })
-      render json: {user: @user, token: token}
+      render json: { user: @user, token: token }
     else
-      render json: {error: "User can not be created."}
+      render json: { error: "User can not be created."}
     end
   end
 
